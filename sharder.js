199788364023cd6729bbfb84bcc4c81aaa7b1b3d
D0wzy <52222574+D0wzy@users.noreply.github.com>
2020-11-16 16:42:51 -0400

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -1,8 +1,8 @@
 const { ShardingManager } = require('discord.js')
-const { shards } = require('config.json')
+const { shards, token } = require('config.json')
 const manager = new ShardingManager({
     file: './index.js',
     totalShards: shards,
     respawn: true,
-    token: 'NzY3Mzc2NTUxOTUxMDczMjgw.X4xBGw.EewxlpHEyG--IWHAkR3Ns7HPBi8'
+    token: token
 })
